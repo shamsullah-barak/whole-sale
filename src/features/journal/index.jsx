df014dd41f779faef9e197ad0663558f2e950777
Shamsullah Barak <cs10shams@gmail.com>
2025-04-28 18:46:05 +0430

    creating and fetching journal entry data

--- src/features/journal/index.jsx
+++ src/features/journal/index.jsx
@@ -10,30 +10,30 @@ import { selectJournals } from "../../store/selectors/journal.selector";
 
 const columns = [
   {
-    field: "name",
-    headerName: "name",
+    field: "status",
+    headerName: "status",
     flex: 0.5,
     minWidth: 80,
   },
   {
-    field: "province",
-    headerName: "province",
+    field: "quantity",
+    headerName: "quantity",
     headerAlign: "center",
     align: "center",
     flex: 1,
     minWidth: 50,
   },
   {
-    field: "district",
-    headerName: "district",
+    field: "description",
+    headerName: "description",
     headerAlign: "center",
     align: "center",
     flex: 1,
     minWidth: 80,
   },
   {
-    field: "accountType",
-    headerName: "account type",
+    field: "date",
+    headerName: "date",
     headerAlign: "center",
     align: "center",
     flex: 1,
@@ -67,77 +67,90 @@ const JournalList = () => {
   };
 
   return (
-    <div
-      style={{
-        height:
-          journals?.journals?.length > 0
-            ? `${
-                Math.min(journals.journals.length, journals.limitPerPage) * 52 +
-                56
-              }px`
-            : "56px", // Just header height when no rows
-        width: "100%",
-        transition: "height 0.3s ease-in-out", // smooth animation
-      }}
-    >
-      <DataGrid
-        rows={journals.journals || []}
-        style={{ cursor: "pointer" }}
-        columns={columns}
-        getRowId={(row) => row.id}
-        onRowClick={handleRowClick}
-        getRowClassName={(params) =>
-          params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
-        }
-        initialState={{
-          pagination: { paginationModel: { pageSize: journals?.limitPerPage } },
-        }}
-        pageSizeOptions={[10, 20, 50]}
-        onPaginationModelChange={(data) => stateChanged(data)}
-        disableColumnResize
-        rowCount={journals?.totalRows}
-        paginationMode="server"
-        pagination
-        page={journals?.currentPage}
-        pageSize={journals?.limitPerPage}
-        loading={journals?.loading}
-        density="compact"
-        slotProps={{
-          filterPanel: {
-            filterFormProps: {
-              logicOperatorInputProps: {
-                variant: "outlined",
-                size: "small",
+    <>
+      {journals.journals.length === 0 ? (
+        <>
+          <div>No Data found</div>
+        </>
+      ) : (
+        <>
+          <DataGrid
+            rows={journals.journals || []}
+            columns={columns}
+            getRowId={(row) => row.id}
+            onRowClick={handleRowClick}
+            getRowClassName={(params) =>
+              params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
+            }
+            initialState={{
+              pagination: {
+                paginationModel: { pageSize: journals?.limitPerPage },
               },
-              columnInputProps: {
-                variant: "outlined",
-                size: "small",
-                sx: { mt: "auto" },
-              },
-              operatorInputProps: {
-                variant: "outlined",
-                size: "small",
-                sx: { mt: "auto" },
-              },
-              valueInputProps: {
-                InputComponentProps: {
-                  variant: "outlined",
-                  size: "small",
+            }}
+            pageSizeOptions={[10, 20, 50]}
+            onPaginationModelChange={(data) => stateChanged(data)}
+            disableColumnResize
+            rowCount={journals?.totalRows}
+            paginationMode="server"
+            pagination
+            page={journals?.currentPage}
+            pageSize={journals?.limitPerPage}
+            loading={journals?.loading}
+            density="compact"
+            slotProps={{
+              filterPanel: {
+                filterFormProps: {
+                  logicOperatorInputProps: {
+                    variant: "outlined",
+                    size: "small",
+                  },
+                  columnInputProps: {
+                    variant: "outlined",
+                    size: "small",
+                    sx: { mt: "auto" },
+                  },
+                  operatorInputProps: {
+                    variant: "outlined",
+                    size: "small",
+                    sx: { mt: "auto" },
+                  },
+                  valueInputProps: {
+                    InputComponentProps: {
+                      variant: "outlined",
+                      size: "small",
+                    },
+                  },
                 },
               },
-            },
-          },
-        }}
-      />
-    </div>
+            }}
+          />
+        </>
+      )}
+    </>
   );
 };
 
 const Journal = () => {
   return (
-    <MainDashboard title="Journal">
+    // <MainDashboard title="Journal">
+    //   <Grid container spacing={2} columns={12} sx={{ width: "100%" }}>
+    //     <Grid xs={12} lg={9} sx={{ width: "100%", textAlign: "right" }}>
+    //       <JournalList />
+    //       <JournalForm />
+    //     </Grid>
+    //   </Grid>
+    // </MainDashboard>
+
+    <MainDashboard title="Journals">
+      {/* <Grid container spacing={2} columns={12} sx={{ width: "100%" }}>
+      <Grid item xs={12} lg={9} sx={{ width: "100%", textAlign: "right" }}>
+        <NavLink to="/products/add">
+          <Button variant="outlined">New Product</Button>
+        </NavLink>
+      </Grid>
+    </Grid> */}
       <Grid container spacing={2} columns={12} sx={{ width: "100%" }}>
-        <Grid xs={12} lg={9} sx={{ width: "100%", textAlign: "right" }}>
+        <Grid xs={12} lg={9} sx={{ width: "100%", height: "100%" }}>
           <JournalList />
           <JournalForm />
         </Grid>
@@ -160,6 +173,9 @@ const financialTerms = [
 ];
 
 const JournalForm = () => {
+  const dispatch = useDispatch();
+  const journals = useSelector(selectJournals);
+
   const [journalEntry, setJournalEntry] = useState({
     description: "",
     quantity: 0,
@@ -179,6 +195,7 @@ const JournalForm = () => {
           },
         }
       );
+      dispatch(fetchJournalsAsync({ page: 1, limit: journals?.limitPerPage }));
       setJournalEntry({
         description: "",
         quantity: 0,
@@ -191,7 +208,7 @@ const JournalForm = () => {
 
   return (
     <form>
-      <Grid container spacing={2}>
+      <Grid container>
         <Grid xs={12} sm={6}>
           <TextField
             select

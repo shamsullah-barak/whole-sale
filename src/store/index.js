484a34e4adabe0ca6bae689d4cbd709b5c6cfb19
Shamsullah Barak <cs10shams@gmail.com>
2025-05-01 14:19:10 +0430

    translation for some words added

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,11 @@ import accountSlice from "./reducers/account.reducer";
 import productReducer from "./reducers/product.reducer";
 import purchaseSlice from "./reducers/purchase.reducer";
 import journalSlice from "./reducers/journal.reducer";
+import appSlice from "./reducers/app.reducer";
 
 export const store = configureStore({
   reducer: {
+    app: appSlice,
     accounts: accountSlice,
     products: productReducer,
     purchases: purchaseSlice,

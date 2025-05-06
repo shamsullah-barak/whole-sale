d8d2d87ea887f1ab72aa449c1d42d131402e5f5e
Shamsullah Barak <cs10shams@gmail.com>
2025-05-06 16:27:36 +0430

    changing language is added

---
+++ src/shared-theme/LanguageSwitcher.js
@@ -0,0 +1,67 @@
+import React, { Fragment, useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useTranslation } from "react-i18next";
+import LanguageIcon from "@mui/icons-material/Language";
+import { useDispatch } from "react-redux";
+import { changeLanguage } from "../store/slices/app.slice";
+
+const LanguageSwitcher = (props) => {
+  const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { i18n } = useTranslation();
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleMode = (language) => () => {
+    i18n.changeLanguage(language);
+    dispatch(changeLanguage({ language }));
+    handleClose();
+  };
+
+  return (
+    <Fragment>
+      <IconButton
+        data-screenshot="toggle-mode"
+        onClick={handleClick}
+        disableRipple
+        size="small"
+        aria-controls={open ? "color-scheme-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        {...props}
+      >
+        <LanguageIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        slotProps={{
+          paper: {
+            variant: "outlined",
+            elevation: 0,
+            sx: {
+              my: "4px",
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem onClick={handleMode("en")}>English</MenuItem>
+        <MenuItem onClick={handleMode("ps")}>Pashto</MenuItem>
+        <MenuItem onClick={handleMode("dr")}>Dari</MenuItem>
+      </Menu>
+    </Fragment>
+  );
+};
+
+export default LanguageSwitcher;

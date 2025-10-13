6e3c58b84fc25bf49a76674c8eadfa8facfdf974
Shamsullah Barak <cs10shams@gmail.com>
2025-10-13 09:54:11 +0430

    sale calculation

--- src/constant/variables.js
+++ src/constant/variables.js
@@ -1,6 +1,10 @@
-const VARIABLES = {
+export const VARIABLES = {
   GIVE_MORE_CASH: "giveMoreCash",
   REDUCE_BALANCE: "reduceBalance",
 };
 
-export default VARIABLES;
+export const PAYMENT_METHODS = {
+  CASH: "cash",
+  CREDIT: "credit",
+  CASH_AND_CREDIT: "cashAndCredit",
+};
